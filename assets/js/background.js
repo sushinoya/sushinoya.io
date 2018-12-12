74fd40134b93bcf798fe5a72d89c6352fd467e8f
Suyash Shekhar <suyashshekhar@gmail.com>
2018-12-12 10:30:38 +0530

    Add blog

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,7 +2,7 @@
 var numOfBgs = 3;
 var bgNumber = Math.floor(Math.random() * numOfBgs) + 1;
 // var imageUrl = './assets/backgrounds/bg'+ bgNumber + '.jpg';
-var imageUrl = './assets/backgrounds/bg' + 5 + '.jpg';
+var imageUrl = './assets/backgrounds/bg' + 6 + '.jpg';
 
 function setBackground() {
     $('#landing').css('background', 'url(' + imageUrl + ')' + 'center center / cover no-repeat');
